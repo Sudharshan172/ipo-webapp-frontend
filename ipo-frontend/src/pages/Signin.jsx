87087267895cc2f0b5e1ce0aca0822529e52cdb7
Sudharshan Reddy <143404435+Sudharshan172@users.noreply.github.com>
2025-06-08 19:32:40 +0530

    Update Signin.jsx

--- ipo-frontend/src/pages/Signin.jsx
+++ ipo-frontend/src/pages/Signin.jsx
@@ -12,7 +12,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/auth/signin/", {
+            const response = await fetch("https://ipo-webapp-backend.onrender.com/auth/signin/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
