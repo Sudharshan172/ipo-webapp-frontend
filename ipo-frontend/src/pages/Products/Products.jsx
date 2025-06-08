8cfae2174b3496592994e759c1c95775f4c27ba1
Sudharshan Reddy <143404435+Sudharshan172@users.noreply.github.com>
2025-06-08 19:53:58 +0530

    Update Products.jsx

--- ipo-frontend/src/pages/Products/Products.jsx
+++ ipo-frontend/src/pages/Products/Products.jsx
@@ -20,7 +20,7 @@ const Products = () => {
             </div>
         </div>
 
-        <div className="bg-green-200 p-8 rounded-lg container mx-auto mt-5 mb-5">
+        <div className="bg-green-200 p-8 rounded-lg max-w-screen-xl mx-auto mt-5 mb-5">
             <h2 className="text-xl font-bold text-gray-800">Diversify Your Portfolio Today!</h2>
             <p className="mt-3 text-md text-gray-600">
                 Start your investment journey with our user-friendly and secure<br />
@@ -28,7 +28,7 @@ const Products = () => {
             </p>
         </div>
 
-        <div className="container mx-auto flex flex-col md:flex-row md:justify-between gap-6 p-5">
+        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:justify-between gap-6 p-5">
             {/* Box 1 - Bluestock IPO */}
             <div className="flex flex-col bg-white border-1 p-6 rounded-lg md:w-1/3">
                 <div className="bg-pink-300 h-10 w-10 mb-4"></div>
@@ -63,7 +63,7 @@ const Products = () => {
             </div>
         </div>
 
-        <div className="bg-violet-200 p-8 rounded-lg container mx-auto mt-5 mb-5">
+        <div className="bg-violet-200 p-8 rounded-lg max-w-screen-xl mx-auto mt-5 mb-5">
             <h2 className="text-xl font-bold text-gray-800">Grow Your Network and Learn the Stock Market</h2>
             <p className="mt-3 text-md text-gray-600">
                 Our community-based approach encourages you to learn from experts<br/>
@@ -71,7 +71,7 @@ const Products = () => {
             </p>
         </div>
 
-        <div className="container mx-auto flex flex-col md:flex-row gap-10 p-5">
+        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row gap-10 p-5">
             {/* Box 1 - Bluestock IPO */}
             <div className="flex flex-col bg-white border-1 p-6 rounded-lg">
                 <div className="bg-violet-200 h-10 w-10 mb-4"></div>
@@ -95,7 +95,7 @@ const Products = () => {
             </div>
         </div>
 
-        <div className="bg-violet-200 p-8 rounded-lg container mx-auto mt-5 mb-5">
+        <div className="bg-violet-200 p-8 rounded-lg max-w-screen-xl mx-auto mt-5 mb-5">
             <h2 className="text-xl font-bold text-gray-800">Make your platform more powerful</h2>
             <p className="mt-3 text-md text-gray-600">
                 Integrate with our in-house trading platform amd build your own<br/>
@@ -103,7 +103,7 @@ const Products = () => {
             </p>
         </div>
 
-        <div className="container mx-auto flex flex-col md:flex-row gap-6 p-5">
+        <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row gap-6 p-5">
             {/* Box 1 - Bluestock IPO */}
             <div className="flex flex-col bg-white border-1 p-6 rounded-lg">
                 <div className="bg-violet-200 h-10 w-10 mb-4"></div>
